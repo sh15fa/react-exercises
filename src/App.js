4bf61b92a5a82238925b6aa3851e8e4595364b43
sh15fa <shahd152000@hotmail.com>
2023-04-12 18:44:05 +0300

    5)exercise10

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-
-import Todo from './Todo';
-import './Style.css';
-
-const DUMMY_TODOS = [
-    'Learn React',
-    'Practice React',
-    'Profit!'
-];
+import React,{useState} from 'react';
 
 // don't change the Component name "App"
 export default function App() {
+    let [dialog,setDialog] = useState(false);
+    const showDialog = ()=>
+    {
+        setDialog(true);
+    }
+    
+    const disableDialog = ()=>
+    {
+        setDialog(false);
+    }
+    
+    
     return (
-        <ul>
-          {DUMMY_TODOS.map(e=><Todo text={e}/>)}
-        </ul>
+      <div>
+        {dialog ?<div id="alert">
+          <h2>Are you sure?</h2>
+          <p>These changes can't be reverted!</p>
+          <button onClick={disableDialog}>Proceed</button>
+        </div> :<p></p>}
+        <button onClick={showDialog}>Delete</button>
+      </div>    
     );
 }
